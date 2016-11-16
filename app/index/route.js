d049e4bd118d0193d31ecee8523aa6f75fe827f9
monsteronfire <monsteronfire@gmail.com>
2016-11-16 16:41:46 +0800

    Start styling sidebar menu

--- app/index/route.js
+++ app/index/route.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
-import fetch from 'ember-network/fetch';
 
 export default Ember.Route.extend({
-  model() {
-    return fetch('https://api.github.com/users/monsteronfire')
-      .then(function(response) {
-        return response.json();
-      });
-  }
 });
